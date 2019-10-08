25c3e49b94efd7e46def8438adbb4658181b1c41
Mohamed AbdElaziz <mohamedmzd0@gmail.com>
2019-10-08 14:17:28 +0200

    data loaded from api

--- lib/activities/Plcaeholder.dart
+++ lib/activities/Plcaeholder.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
 
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class Placeholder extends StatefulWidget {
   String data;
@@ -15,6 +19,10 @@ class Placeholder extends StatefulWidget {
 }
 
 class _PlaceholderState extends State<Placeholder> {
+  List<Results> _list = null;
+  RefreshController _refreshController = new RefreshController();
+  int page = 1;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -24,13 +32,71 @@ class _PlaceholderState extends State<Placeholder> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SmartRefresher(
+        onRefresh: getTopRated,
+        onLoading: _loadmore,
+        enablePullDown: true,
+        controller: _refreshController,
+        enablePullUp: true,
+        child: GridView.count(
+            padding: EdgeInsets.all(1),
+            crossAxisCount: 2,
+            children: List.generate(_list == null ? 0 : _list.length, (index) {
+              return GestureDetector(
+                onTap: () => print("dddd${index}"),
+                child: Card(
+                  borderOnForeground: true,
+                  elevation: 2,
+                  color: Colors.white,
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        ExtendedImage.network(
+                          "https://image.tmdb.org/t/p/w185/${_list[index].poster_path}",
+                          fit: BoxFit.fill,
+                          cache: true,
+                          scale: 2,
+                          border: Border.all(color: Colors.red, width: 1.0),
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                        ),
+                        Text(
+                          '${_list[index].original_title}',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(5),
+                        ),
+                        Text(
+                          '${_list[index].release_date}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            })),
+      ),
+    );
   }
 
-  Future<Results> getTopRated() async {
+  Future<List<Results>> getTopRated() async {
     var URL = "http://api.themoviedb.org/3/movie/" +
         widget.data +
-        "?api_key=832f13a97b5d2df50ecf0dbc8a0f46ae";
+        "?api_key=832f13a97b5d2df50ecf0dbc8a0f46ae&page=${page}";
     http.get(URL).then((http.Response response) {
       print(response.body);
       final int statusCode = response.statusCode;
@@ -39,8 +105,22 @@ class _PlaceholderState extends State<Placeholder> {
         throw new Exception("Error while fetching data");
       }
       MovieResponse res = MovieResponse.fromJson(jsonDecode(response.body));
+      setState(() {
+        if (_list == null)
+          _list = res.results;
+        else
+          _list.addAll(res.results);
+      });
+      _refreshController.refreshCompleted();
+      _refreshController.loadComplete();
       return res.results;
     });
+  }
+
+  void _loadmore() {
+    page++;
+    getTopRated();
+    print('load more');
   }
 }
 
@@ -51,8 +131,7 @@ class MovieResponse {
   MovieResponse({this.page, this.results});
 
   factory MovieResponse.fromJson(Map<String, dynamic> json) {
-    return MovieResponse(
-        page: json["page"], results: getResults(json));
+    return MovieResponse(page: json["page"], results: getResults(json));
   }
 
   static List<Results> getResults(json) {
